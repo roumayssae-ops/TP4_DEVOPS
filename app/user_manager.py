094a6e3dd1e76d9cd9df26db1c6884e12df12485
roumayssae-ops <roumayssaetk@gmail.com>
2026-03-10 12:43:54 +0000

    fix flake8 errors

--- app/user_manager.py
+++ app/user_manager.py
@@ -1,21 +1,27 @@
-class UserManager: 
+class UserManager:
+
+ 
     def __init__(self):
-        self.users=[] 
+        self.users=[]
+
 
     def add_user(self,username):
         if not username:
             raise ValueError("Le nom d'utilisateur est obligatoire") 
         if username in self.users:
             raise ValueError("Utilisateur déjà existant")
-        self.users.append(username) 
+        self.users.append(username)
 
-    def remove_user(self, username):
+
+    def remove_user(self,username):
         if username not in self.users:
             raise ValueError("Utilisateur introuvable")
-        self.users.remove(username) 
+        self.users.remove(username)
+
 
     def count_users(self):
-        return len(self.users) 
-    
+        return len(self.users)
+
+
 def count_total_Users(users):
     return len(users)
